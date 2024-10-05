59d08971882eea838384ed5e4e55a55f1d2b74c7
leonardo <leonardozalazar89@gmail.com>
2024-10-05 15:12:55 -0300

    Correccion de Test de CrearCurso y Curso

--- core/src/test/java/usecase/TestCrearCurso.java
+++ core/src/test/java/usecase/TestCrearCurso.java
@@ -13,36 +13,35 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TestCrearCurso{
     @Mock
     Persistencia BD;
     @Test
-    void GuardarCurso() {
+    void CursoConElDistintoNombreSeGuardaElCurso() {
         CrearCurso micurso = new CrearCurso(BD);
+        when(BD.existeCurso("Matematicas")).thenReturn(false);// ese curso no existe
         when(BD.guardarCurso(Mockito.any())).thenReturn(true);
-        Assertions.assertTrue(micurso.crearCurso("Matematicas",LocalDate.of(2025,8,15), Nivel.MEDIO));
+        Assertions.assertDoesNotThrow(() -> micurso.crearCurso("Matematicas", LocalDate.MAX, Nivel.MEDIO));
+        Assertions.assertTrue(micurso.crearCurso("Matematicas", LocalDate.MAX, Nivel.MEDIO));
     }
     @Test
-    void NoSePuedeGuardarCurso() {
+    void CursoConElDistintoNombreNoSeGuardaElCurso() {
         CrearCurso micurso = new CrearCurso(BD);
+        when(BD.existeCurso("Matematicas")).thenReturn(false);// ese curso no existe
         when(BD.guardarCurso(Mockito.any())).thenReturn(false);
-        Assertions.assertFalse(micurso.crearCurso("Matematicas",LocalDate.of(2025,8,15), Nivel.MEDIO));
+        Assertions.assertDoesNotThrow(() -> micurso.crearCurso("Matematicas", LocalDate.MAX, Nivel.MEDIO));
+        Assertions.assertFalse(micurso.crearCurso("Matematicas", LocalDate.MAX, Nivel.MEDIO));
     }
     @Test
     void CursoConElMismoNombre() {
         CrearCurso micurso = new CrearCurso(BD);
-        when(BD.existeCurso("Matematicas")).thenReturn(true);
-        Assertions.assertThrows(ExceptionCursoMismoNombre.class,()-> micurso.crearCurso("Matematicas", LocalDate.of(2025,8,15), Nivel.MEDIO));
+        Exception e;
+        when(BD.existeCurso("Matematicas")).thenReturn(true); // ese curso ya existe
+        e = Assertions.assertThrows(ExceptionCursoMismoNombre.class,()-> micurso.crearCurso("Matematicas", LocalDate.MAX, Nivel.MEDIO));
+        verify(BD,never()).guardarCurso(Mockito.any());
+        Assertions.assertEquals("Ya existe curso con este nombre: Matematicas",e.getMessage());
     }
-    @Test
-    void CursoConElDistintoNombre() {
-        CrearCurso micurso = new CrearCurso(BD);
-        when(BD.existeCurso("Matematicas")).thenReturn(false);
-        Assertions.assertDoesNotThrow(() -> micurso.crearCurso("Matematicas", LocalDate.MAX, Nivel.MEDIO));
-    }
-
 }
